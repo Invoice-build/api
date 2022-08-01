17dac930cf0110a0fb8abb40ba854c69dcc94d40
Gareth Fuller <gareth.a.f@gmail.com>
2022-08-01 16:28:12 +0100

    Start using a namespace in redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,13 @@
 Sidekiq.configure_client do |config|
-  config.redis = { url: "#{ENV['REDIS_URL']}" }
+  config.redis = {
+    url: "#{ENV['REDIS_URL']}",
+    namespace: 'invoice_build'
+  }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "#{ENV['REDIS_URL']}" }
+  config.redis = {
+    url: "#{ENV['REDIS_URL']}",
+    namespace: 'invoice_build'
+  }
 end
